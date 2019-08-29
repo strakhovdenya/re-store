370f1ea7a3a8357777bbe770d354553d07fff476
Denis <strakhov.denya@gmail.com>
2019-08-29 22:08:22 +0300

    add base start app

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -1,5 +1,16 @@
 export default class BookstoreService {
     getBooks() {
-        return [];
+        return [
+            {
+                id:1,
+                title:'book_1',
+                author:'author_1'
+            },
+            {
+                id:2,
+                title:'book_2',
+                author:'author_2'
+            }
+        ];
     }
 }
